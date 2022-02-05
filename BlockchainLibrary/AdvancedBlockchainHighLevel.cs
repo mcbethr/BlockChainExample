ac3102c69d8e31c12f84c905a57ae5e4faf1c61d
Ryan McBeth <ryan.mcbeth@gmail.com>
2022-02-05 09:40:24 -0500

    Added in an Else for the tampering of the firstblock

--- BlockchainLibrary/AdvancedBlockchainHighLevel.cs
+++ BlockchainLibrary/AdvancedBlockchainHighLevel.cs
@@ -167,6 +167,10 @@ namespace BlockchainLibrary.ChainOperations
                 {
                     return true;
                 }
+                else 
+                { 
+                    return false;
+                }
             }
 
             //Do special Accounting for the last block
